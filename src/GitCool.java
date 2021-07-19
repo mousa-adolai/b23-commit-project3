dbbb2d544cc9575d51b81869722419c9e50fc0e4
Mousa Adolai <mousa.adolai@gmail.com>
2021-07-19 16:42:15 -0400

    added a line to gitcool class

--- src/GitCool.java
+++ src/GitCool.java
@@ -4,5 +4,6 @@ public class GitCool {
     public static void main(String[] args) {
         java.util.Scanner input = new Scanner(System.in);
 //I AM COOL
+        System.out.println("Tell me your secrets");
     }
 }
