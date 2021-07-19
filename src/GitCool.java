2a52145affd6622bbbe44f711ffa369604921bd0
Mousa Adolai <mousa.adolai@gmail.com>
2021-07-19 16:43:14 -0400

    edit the comment from a commit message and wrap a line

--- src/GitCool.java
+++ src/GitCool.java
@@ -5,5 +5,8 @@ public class GitCool {
         java.util.Scanner input = new Scanner(System.in);
 //I AM COOL
         System.out.println("Tell me your secrets");
+        String secrets = input.nextLine();
+
+        System.out.println("HA HA HA now I will push your secrets to github");
     }
 }
